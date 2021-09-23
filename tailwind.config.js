491ce6501d3d6b240943b96b570425167a4f1ab3
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-09-23 15:22:51 -0400

    Add config for aspect ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    aspectRatio: { 
+      'none': 0,
+      'square': [1, 1],
+      'landscape': [16, 9],
+      'portrait': [9, 16]
+    },
     extend: {},
   },
   variants: {
